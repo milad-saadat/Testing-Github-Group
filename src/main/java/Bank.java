924c7aaf6f3f21a923cb4099bf78b4111526687c
Parham <parhampch@gmail.com>
2020-04-16 01:27:14 +0430

    test commit num1

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -3,10 +3,14 @@ import java.util.ArrayList;
 public class Bank {
     private static ArrayList<Bank> allBanks = new ArrayList<Bank>();
     String name ;
+    String bankID;
 
     public Bank(String name) {
         this.name = name ;
         allBanks.add(this) ;
     }
 
+    public String getBankID() {
+        return bankID;
+    }
 }
