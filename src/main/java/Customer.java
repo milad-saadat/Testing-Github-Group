aa714d1f581efd977e70fe6043992e6cad5abdc9
faraziq <faraz.ghahremani@yahoo.com>
2020-04-17 16:19:15 +0430

    add print good methode

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -2,6 +2,10 @@ public class Customer
 {
     private String name;
 
+    private void sayHello(){
+        System.out.println("hello");
+    }
+
     public String getName() {
         return name;
     }
